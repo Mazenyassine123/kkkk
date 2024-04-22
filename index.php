4657ebab8bf045b0f49e59dc54d7220bbad939a3
Mazenyassine123 <167718262+Mazenyassine123@users.noreply.github.com>
2024-04-22 14:39:18 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,93 @@
+<html>
+<body>
+
+<h1>ONLINE SHOOPING </h1></br></br></br></br></br>
+<button id="addmin"><a   href="addmin.php">  ADDMIN  </a></button> </br></br></br></br>
+<button id="customer"><a  href="add_customer.php">CUSTOMER </a></button> 
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
+
+<style>
+
+h1{
+width : 400px;
+margin-left : 500px;
+border : 3px solid;
+border-radius :10px ;
+text-align : center;
+
+
+	
+	
+	
+	
+}
+
+body{
+	
+	
+	background-image : url("img/intro-bg_1.jpg");
+	background-repeat : no-repeat;
+	background-size:cover;
+    background-attachment : fixed;
+	background-position: center;
+	
+	
+}
+
+
+#addmin , #customer{
+	
+	
+	width: 165px;
+     height : 50px;
+	border:1px solid;	
+	border-radius : 10px;
+	margin-top : 100px;
+	margin-left : 600px;
+	
+text-align : center;
+	background-color:crimson;
+	
+	
+	
+}
+
+a{
+	
+	
+	font-size: 25px;
+	text-decoration:none;
+	color: black;
+	
+	
+}
+
+
+#addmin:hover , #customer:hover{
+	
+	
+	background-color:green;
+	
+	
+	
+}
+
+a:hover{
+	
+	color:crimson;
+	
+}
+
+</style>
